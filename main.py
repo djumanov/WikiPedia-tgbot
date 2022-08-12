80c9a985d45acf44209176069e285d16df869a62
djumanov <diyorbekjumanov1120@gmail.com>
2022-08-12 21:07:23 +0500

    test echo

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from telegram.bot import Bot
+from telegram import User
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.update import Update
+
 
 TOKEN = "5598770871:AAFm0H64aLx5kI0n1CNFNwNJTT5kXmLa7bw"
 
-bot = Bot(token=TOKEN)
 
-print(bot.get_updates())
+updater: User = Updater(token=TOKEN)
+
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Hi")
+
+
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+
+
+updater.start_polling()
+updater.idle()
