c6734c1bf995871e64fb2f031070dc677a1c9486
djumanov <diyorbekjumanov1120@gmail.com>
2022-08-12 21:52:14 +0500

    search command

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram import User
+from telegram import User, ForceReply
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from telegram.update import Update
 
@@ -10,11 +10,17 @@ updater: User = Updater(token=TOKEN)
 
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text(f"Assalomu alaykum, {update.message.from_user.first_name}!\n\nWiki Pedia Botga Hush kelibsiz!!!\nWiki Pediadan qidirish uchun:\n\t```/search something```\nbuyrug'idan foydalaning...")
+    update.message.reply_text(f"Assalomu alaykum, {update.message.from_user.first_name}!\n\nWiki Pedia Botga Hush kelibsiz!!!\nWiki Pediadan qidirish uchun:\n\t'/search something'\nbuyrug'idan foydalaning...")
+
+
+def search(update: Update, context: CallbackContext):
+    print(update.message.text)
+
 
 
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('search', search))
 
 
 updater.start_polling()
